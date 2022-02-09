d620d15279627cfe1ab17af55eddf3fb34490324
Siva Rajana <30532819+SivaRajana@users.noreply.github.com>
2022-02-09 16:24:17 +0530

    Easy Problem

---
+++ numberPatternWitoutNestedLoops.py
@@ -0,0 +1,12 @@
+N = int(input())
+string = ""
+for i in range(1, N+1):
+    string = string + str(i) + " "
+for j in range(1,N+1):
+    print (string)
+   
+# 1 2 3 4 5 
+# 1 2 3 4 5 
+# 1 2 3 4 5 
+# 1 2 3 4 5 
+# 1 2 3 4 5 
