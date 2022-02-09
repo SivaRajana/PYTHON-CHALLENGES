5d3eb2f37c6ed04bd4c879397d1eec6ceb9c9e64
Siva Rajana <30532819+SivaRajana@users.noreply.github.com>
2022-02-09 16:30:04 +0530

    very easy

---
+++ numberRectanglePattern.py
@@ -0,0 +1,10 @@
+M = int(input())
+N = int(input())
+pattern = ""
+for i in range(1, N+1):
+    pattern = pattern + str(i) + " "
+for j in range(1, M+1):
+    print (pattern)
+    
+# 1 2 3 
+# 1 2 3 
