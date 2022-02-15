317e755d93f901b2b4c8a6c06a477bdf971b99bd
Siva Rajana <30532819+SivaRajana@users.noreply.github.com>
2022-02-15 15:50:08 +0530

    Create revers the words in sentence.py

---
+++ revers the words in sentence.py
@@ -0,0 +1,6 @@
+given_sentence = input()
+list_of_words = given_sentence.split()
+reversed_words_list = []
+for i in list_of_words:
+    reversed_words_list += [i[::-1]]
+print (" ".join(reversed_words_list))
