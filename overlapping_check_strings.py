33c58977ffc2f62cfc7b11be5f1d91cb9ccc8623
Siva Rajana <30532819+SivaRajana@users.noreply.github.com>
2022-02-22 11:26:59 +0530

    tricky

---
+++ overlapping_check_strings.py
@@ -0,0 +1,9 @@
+string_a = input()
+string_b = input()
+result = "No overlapping"
+for i in range(len(string_a), 0, -1):
+    string_test = string_a[(-1 * i)::]
+    if (string_b.startswith(string_test)):
+        result = string_test
+        break
+print (result)
