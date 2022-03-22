f4ec2b08b8e7e4f8461b667dcc53be6b769578a6
Siva Rajana <30532819+SivaRajana@users.noreply.github.com>
2022-03-22 14:48:30 +0530

    ++method

--- invertedFullPyramidNumbers.py
+++ invertedFullPyramidNumbers.py
@@ -5,3 +5,11 @@ for i in range(N):
     for j in range(N-i):
         pattern = pattern + str(j+1) + " "
     print (spaces + pattern)
+
+#METHOD 2
+N = int(input())
+for row in range(N,0,-1):
+    print (" " * (N-row), end="")
+    for col in range(1, row+1):
+        print (col, end=" ")
+    print ()
