a261cdd97e946c18e3965eeda289ae9ff22c0879
Siva Rajana <30532819+SivaRajana@users.noreply.github.com>
2022-03-22 11:49:01 +0530

    easy method to solve it got added

--- commonElementsOfStrings.py
+++ commonElementsOfStrings.py
@@ -8,3 +8,13 @@ for i in result_set:
     list_final.append(int(i))
 list_final.sort()
 print (list_final)
+
+#second way of solving it
+string1=input()
+string2=input()
+
+set_a = set(string1)
+set_b = set(string2)
+
+result = sorted(set_a & set_b)
+print (*result)
