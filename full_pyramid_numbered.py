9e4a47a45d9a55aa03212641db06c8b62e6e62bc
Siva Rajana <30532819+SivaRajana@users.noreply.github.com>
2022-03-22 12:53:42 +0530

    ++ Easy way

--- full_pyramid_numbered.py
+++ full_pyramid_numbered.py
@@ -4,8 +4,19 @@ for i in range(1, N+1):
     for j in range(1, i+1):
         line_pattern = line_pattern + str(j) + " "
     print(line_pattern)
+    
+    
 #     1 
 #    1 2 
 #   1 2 3 
 #  1 2 3 4 
 # 1 2 3 4 5 
+
+n = int(input())
+start = 1
+for row in range(1, n+1):
+    print (" " * (n-row), end="")
+    for col in range(1, row+1):
+        print (start, end=" ")
+        start += 1
+    print ()
