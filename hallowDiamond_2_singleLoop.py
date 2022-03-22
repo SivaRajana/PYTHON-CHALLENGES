42a5d6defb7693b06613c1da2641c0197978d4aa
Siva Rajana <30532819+SivaRajana@users.noreply.github.com>
2022-03-22 14:31:22 +0530

    Update hallowDiamond_2_singleLoop.py

--- hallowDiamond_2_singleLoop.py
+++ hallowDiamond_2_singleLoop.py
@@ -5,7 +5,7 @@ for row in range(total_rows):
     if (row < N):
         row = (N - row)
     else:
-        row = (row % N) + 1
+        row = (row % N) + 1 # this step avoid creating chaos of when 10%2 ==  0 like that, if we iterate from 0 to 2*n, then 10 won't come into picture.
     stars = row * "* "
     hallow_spaces = 4 * (N-row)
     line_pattern = stars + (hallow_spaces * " ") + stars
