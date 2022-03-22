eea33a86ae55b263b9510aa621f6b1d238c1b7a7
Siva Rajana <30532819+SivaRajana@users.noreply.github.com>
2022-03-22 15:48:38 +0530

    ++ fmodified the indexing and slicing logic

--- overlapping_check_strings.py
+++ overlapping_check_strings.py
@@ -7,3 +7,14 @@ for i in range(len(string_a), 0, -1):
         result = string_test
         break
 print (result)
+
+#complexity decreased
+string_a = input()
+string_b = input()
+result = "No overlapping"
+for i in range(len(string_a)):
+    string_test = string_a[i::]
+    if (string_b.startswith(string_test)):
+        result = string_test
+        break
+print (result)
