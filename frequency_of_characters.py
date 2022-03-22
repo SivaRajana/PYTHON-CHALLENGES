c72e0ea15800c4d37ba2d19343c016895a202774
Siva Rajana <30532819+SivaRajana@users.noreply.github.com>
2022-03-22 12:36:58 +0530

    using dictionary's method ++

--- frequency_of_characters.py
+++ frequency_of_characters.py
@@ -6,3 +6,16 @@ set_a.discard(" ")
 list_unique_alphabets = sorted(set_a)
 for item in list_unique_alphabets:
     print ("{}: {}".format(item,list_a.count(item)))
+    
+#----------------------------------------------------------    
+string = input()
+dict_a = {}
+for char in string:
+    if char not in dict_a:
+        dict_a[char] = 1
+        continue
+    dict_a[char] += 1
+del dict_a[" "]
+for key in sorted(dict_a):
+    print (key, "---", dict_a[key])
+    
