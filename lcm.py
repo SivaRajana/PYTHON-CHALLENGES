dcf3b57e592c7fd108a1632040de4bfe32b932dc
Siva Rajana <30532819+SivaRajana@users.noreply.github.com>
2022-03-22 15:01:00 +0530

    ++ one more way

--- lcm.py
+++ lcm.py
@@ -10,3 +10,20 @@ while (lcm_not_found):
         lcm_not_found = False
     biggest = biggest + 1
 print (biggest-1)
+
+
+#-----------------------------------
+
+numbers = input().split()
+m,n = int(numbers[0]), int(numbers[1])
+
+big = m
+if (n > big):
+    big = n
+
+while(big):
+    if (big % m == 0 and big % n == 0):
+        break
+    big += 1
+
+print (big)
