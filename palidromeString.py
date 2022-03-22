c55feb453b971884e54740454c82cc9dab2f0925
Siva Rajana <30532819+SivaRajana@users.noreply.github.com>
2022-03-22 15:54:47 +0530

    ++ ord() function used to eliminate non-alphabets

--- palidromeString.py
+++ palidromeString.py
@@ -8,3 +8,13 @@ for i in string:
     if (i != " " and i != "'" and i != " \""):
         modified_string = i + modified_string
 print (modified_string == modified_string[::-1])
+
+#-------------------------------
+# Perfect Method to solve this problem
+string = input()
+string = string.lower()
+modified_string =""
+for i in string:
+    if (ord(i) >= 65 and ord(i)<=90) or (ord(i)>= 97 and ord(i)<=122):#removing the spaces and special chars
+        modified_string = i + modified_string
+print (modified_string == modified_string[::-1])
