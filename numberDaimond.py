57d9a1bac73afb792fe7f80a511ae239d2f97123
Siva Rajana <30532819+SivaRajana@users.noreply.github.com>
2022-03-22 15:34:20 +0530

    ++ method

--- numberDaimond.py
+++ numberDaimond.py
@@ -11,3 +11,17 @@ for i in range(1, (N * 2)):
     for j in range(1, number_of_elements + 1):
         string = string + str(j) + " "
     print (left_spaces + string)
+# ------------------ METHOD 2 --------------------------------------------
+N = int(input())
+for i in range(1,N+1):
+    print (" " * (N-i), end="")
+    for j in range(1, i+1):
+        print (j, end=" ")
+    print ()
+
+for i in range(N-1, 0, -1):
+    print (" " * (N-i), end="")
+    for j in range(1, i+1):
+        print (j, end=" ")
+    print()
+    
